3c7e0da75a41e7a7276a816f8e8679d4933e6276
Subham Khadka <subhamkhadka99@gmail.com>
2026-04-01 14:34:30 +0545

    Created clerk webhook controller

---
+++ server/src/controllers/webhook.controller.js
@@ -0,0 +1,62 @@
+import { Webhook } from "svix";
+import User from "../models/User.model.js";
+
+// API Controller function to manage clerk user with db
+
+const clerkWebhooks = async (req, res) => {
+  try {
+    // Create a Svix instance with clerk webhool secret
+    const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+
+    // Verifying Headers
+    await whook.verify(JSON.stringify(req.body), {
+      "svix-id": req.headers["svix-id"],
+      "svix-timestampp": req.headers["svix-timestamp"],
+      "svix-signature": req.headers["svix-signature"],
+    });
+
+    // getting data from req body
+    const { data, type } = req.body;
+
+    // Switch Case for different Events
+    switch (type) {
+      case "user.created": {
+        const userData = {
+          _id: data.id,
+          email: data.email_addresses[0].email_address,
+          name: data.first_name + " " + data.last_name,
+          image: data.image_url,
+          resume: "",
+        };
+        await User.create(userData);
+        res.json({});
+        break;
+      }
+
+      case "user.updated": {
+        const userData = {
+          email: data.email_addresses[0].email_address,
+          name: data.first_name + " " + data.last_name,
+          image: data.image_url,
+        };
+        await User.findByIdAndUpdate(data.id, userData);
+        res.json({});
+        break;
+      }
+
+      case "user.deleted": {
+        await User.findByIdAndDelete(data.id);
+        res.json({});
+        break;
+      }
+
+      default:
+        break;
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.json({ success: false, message: "Webhooks Error" });
+  }
+};
+
+export default clerkWebhooks
