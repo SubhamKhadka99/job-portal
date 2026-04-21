b664cd16c97771abee045e24776fbddb9f70ab50
Subham Khadka <subhamkhadka99@gmail.com>
2026-04-21 18:06:37 +0545

    add core dependencies and configuration files

--- server/src/config/instrument.js
+++ server/src/config/instrument.js
@@ -1,9 +1,9 @@
-// Import with `` if you are using ESM
+// Import with `import` if you are using ESM
 import * as Sentry from "@sentry/node";
 import { nodeProfilingIntegration } from "@sentry/profiling-node";
 
 Sentry.init({
-  dsn: "https://f6de0b868403d1650df7f4e6a308123d@o4511133633282048.ingest.us.sentry.io/4511133641146368",
+  dsn: process.env.SENTRY_DSN,
   integrations: [nodeProfilingIntegration(), Sentry.mongooseIntegration()],
   //! tracing
   tracesSampleRate: 1.0,
