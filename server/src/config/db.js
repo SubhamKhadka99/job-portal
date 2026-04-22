e9390979debd69b15ec8202b73726791f45119f0
Subham Khadka <subhamkhadka99@gmail.com>
2026-04-22 14:00:56 +0545

    fixed db error

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 
-//! function to connect to MongoDB database
-
 const connectDB = async () => {
   mongoose.connection.on("connected", () => console.log("Database connected"));
 
-  await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`);
+  await mongoose.connect(`${process.env.MONGODB_URI}/job-portal`, {
+    tls: true,
+    tlsAllowInvalidCertificates: false,
+  });
 };
 
 export default connectDB;
